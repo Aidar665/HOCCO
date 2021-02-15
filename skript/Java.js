0791668a2d5af973d7676a12ec461cc0588fdfe4
Aidar665 <74964389+Aidar665@users.noreply.github.com>
2021-02-15 14:56:57 +0300

    Add files via upload

--- skript/Java.js
+++ skript/Java.js
@@ -1,6 +1,17 @@
-var navbar = document.querySelector(".navbar")
-var ham = document.querySelector(".ham")
-ham.addEventListener("click", toggleHamburger)
-function toggleHamburger(){
-  navbar.classList.toggle("showNav")
-  ham.classList.toggle("showClose")}
+let burger  = document.querySelector('.burger');
+let overlay = document.querySelector('.overlay');
+let body = document.querySelector('body');
+
+let links = document.querySelectorAll('.menu__link');
+
+links.forEach(function(element){
+  element.addEventListener('click' , toggleMenu);
+})
+
+function toggleMenu(){
+  burger.classList.toggle('burger--active');
+  overlay.classList.toggle('overlay--active');
+  body.classList.toggle('body--active-menu');
+}
+
+burger.addEventListener('click' , toggleMenu);
